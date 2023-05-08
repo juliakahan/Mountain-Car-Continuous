64b31116caccbacedeb322e5f0f7dd442908835a
Kamil Bernacik <bernacikkamil@gmail.com>
2023-05-08 22:24:42 +0200

    make progres bar

--- mcc_learn.py
+++ mcc_learn.py
@@ -1,6 +1,6 @@
 import gymnasium as gym
 import numpy as np
-import matplotlib.pyplot as plt
+from tqdm import tqdm
 import pickle
 
 from utility import *
@@ -8,11 +8,10 @@ from utility import *
 if __name__ == '__main__':
     num_of_episodes = 5000
     num_of_steps_per_episode = 1000
-    alpha = 0.01  # discount parameter
+    alpha = 0.1  # discount parameter
     gamma = 0.9  # discount parameter
     eps = 1.  # exploration parameter
 
-    # env = gym.make('MountainCar-v0', render_mode="human")
     env = gym.make('MountainCar-v0', max_episode_steps=num_of_steps_per_episode)
 
     states = list()
@@ -29,6 +28,10 @@ if __name__ == '__main__':
         for action in action_space:
             Q[state, action] = 0
 
+    # create progress bar
+    progress_bar = tqdm(total=num_of_episodes, desc='Learning')
+
+    # learning loop
     for episode in range(num_of_episodes):
         terminated = False
         truncated = False
@@ -60,51 +63,23 @@ if __name__ == '__main__':
         scores[episode] = score
         epsilons[episode] = eps
 
-        mean_score = np.mean(scores[max(0, episode - 100):(episode + 1)])
+        # per one hundred episodes calculate mean score,
+        # which is shown on the progress bar
         if episode % 100 == 0:
-            print("Episode:", episode, "Score:", score, "epsilon:", eps)
-            print("Mean score over last 100 episodes:", mean_score)
+            # mean of last 100 episodes
+            mean = np.mean(scores[max(0, episode - 100):(episode + 1)])
 
+        # Update the progress bar
+        progress_bar.set_postfix(epsilon=f'{eps:.2f}',
+                                 score=str(int(score)),
+                                 mean_score=str(int(mean)))
+        progress_bar.update(1)
 
     env.close()
 
-    fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, figsize=(20, 12), sharex=True)
+    plot(scores, epsilons, num_of_episodes)
 
-    ax1.plot(scores, color='blue')
-    ax1.set_xlabel('Episode', fontsize=16)
-    ax1.set_ylabel('Score', color='blue', fontsize=16)
-    ax1.tick_params(axis='y', labelcolor='blue', labelsize=14)
-    ax1.set_ylim(-1000, 0)
-    ax1.set_yticks(range(-1000, 1, 100))
-    ax1.set_xlim(0, num_of_episodes)
-    ax1.set_xticks(np.arange(0, num_of_episodes, 100))
-    ax1.grid(alpha=0.4)
-
-    ax2.plot(epsilons, color='red', linewidth=3)
-    ax2.set_ylabel('Epsilon', color='red', fontsize=16)
-    ax2.tick_params(axis='y', labelcolor='red', labelsize=14)
-    ax2.set_ylim(0, 1)
-    ax2.grid(alpha=0.4)
-
-    mean_scores = [np.mean(scores[max(0, i - 99):(i + 1)]) for i in range(len(scores))]
-    ax3 = ax1.twinx()
-    ax3.plot(mean_scores, color='green', linewidth=3)
-    ax3.set_ylabel('Mean score (last 100 episodes)', color='green', fontsize=16)
-    ax3.tick_params(axis='y', labelcolor='green', labelsize=14)
-    ax3.set_ylim(-1000, 0)
-    ax3.set_yticks(range(-1000, 1, 100))
-    ax3.spines['right'].set_visible(False)
-    ax3.spines['left'].set_visible(False)
-    ax3.yaxis.set_label_position('right')
-    ax3.yaxis.tick_right()
-
-    plt.title('num_of_episodes = ' + str(num_of_episodes), fontsize=20, color='green')
-
-    plt.tight_layout()
-    plt.savefig('scores_mcc.png')
-    plt.show()
-
-
+    save_csv(scores, epsilons, num_of_episodes)
 
     f = open("mountaincar.pkl", "wb")
     pickle.dump(Q, f)
--- utility.py
+++ utility.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import csv
 
 # number of buckets for position and velocity
 pos_bucket_numer = 20
@@ -32,3 +34,50 @@ def max_action(Q, state, actions=action_space):
     action = np.argmax(values)
 
     return action
+
+# credit: J.K.
+def plot(scores, epsilons, num_of_episodes):
+    fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, figsize=(20, 12), sharex=True)
+
+    ax1.plot(scores, color='blue')
+    ax1.set_xlabel('Episode', fontsize=16)
+    ax1.set_ylabel('Score', color='blue', fontsize=16)
+    ax1.tick_params(axis='y', labelcolor='blue', labelsize=14)
+    ax1.set_ylim(-1000, 0)
+    ax1.set_yticks(range(-1000, 1, 100))
+    ax1.set_xlim(0, num_of_episodes)
+    ax1.set_xticks(np.arange(0, num_of_episodes, 100))
+    ax1.grid(alpha=0.4)
+
+    ax2.plot(epsilons, color='red', linewidth=3)
+    ax2.set_ylabel('Epsilon', color='red', fontsize=16)
+    ax2.tick_params(axis='y', labelcolor='red', labelsize=14)
+    ax2.set_ylim(0, 1)
+    ax2.grid(alpha=0.4)
+
+    mean_scores = [np.mean(scores[max(0, i - 99):(i + 1)]) for i in range(len(scores))]
+
+    ax3 = ax1.twinx()
+    ax3.plot(mean_scores, color='green', linewidth=3)
+    ax3.set_ylabel('Mean score (last 100 episodes)', color='green', fontsize=16)
+    ax3.tick_params(axis='y', labelcolor='green', labelsize=14)
+    ax3.set_ylim(-1000, 0)
+    ax3.set_yticks(range(-1000, 1, 100))
+    ax3.spines['right'].set_visible(False)
+    ax3.spines['left'].set_visible(False)
+    ax3.yaxis.set_label_position('right')
+    ax3.yaxis.tick_right()
+
+    plt.title('num_of_episodes = ' + str(num_of_episodes), fontsize=20, color='green')
+
+    plt.tight_layout()
+    plt.savefig('scores_mcc.png')
+    plt.show()
+
+def save_csv(scores, epsilons, num_of_episodes):
+    data = zip(scores, epsilons)
+
+    with open('scores_epsilons.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(['Score', 'Epsilon'])
+        writer.writerows(data)
